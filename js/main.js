2d37ef1bec412dc35bb5a36c960891279eba7e98
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-23 16:00:10 +0100

    drop visualizzato e scompare al click in pagina

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 function dropdown() {
-
-    $('.items').hover(function () {
-        $('.drop').slideDown();
-
-    })
-
+    $('.items').hover(function () {//proviamo ad aggiungere this per selez uno solo 
+        var item = $(this);
+        var drop = item.find($('.drop'));
+        var visible = drop.is(':visible');
+        drop.addClass('active');
+        
     
 
+    });
+    
+    
+    // forse un po meglio ma nn mi convince
 
 }
 
@@ -14,7 +18,7 @@ function reset() {
 
     $('body').click(function () {
 
-        $('.drop').slideUp();
+        $('.drop').removeClass('active');
     })
 }
 
