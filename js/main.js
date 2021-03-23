9b5029c8f4a60df0485f5ec30c0ba72f27c48e4c
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-23 17:29:02 +0100

    ok

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ function dropdown() {
         var item = $(this);
         var drop = item.find($('.drop'));
         drop.addClass('active');
-        item.next().find($('.drop')).removeClass('active');
-        item.prev().find($('.drop')).removeClass('active');
+        item.next().find('.drop').removeClass('active');
+        item.prev().find('.drop').removeClass('active');
 
     }); // forse un po meglio ma nn mi convince
 
